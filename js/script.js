e4bf62cc7420eb51fde9991dc25b855cbd5020b2
Kartik Hatwar <kartikhatwar98@gmail.com>
2022-07-01 18:06:49 +0530

    using api

--- js/script.js
+++ js/script.js
@@ -35,3 +35,50 @@ formClose.addEventListener('click' , ()=>{
 function rangeSlide(value) {
     document.getElementById('rangeValue').innerHTML = value;
 }
+
+
+const API_KEY = 'api_key=e272f317f8df98d65d0f955c6dc2b70d';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const PAPI_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+const main = document.getElementById('umovies');
+
+
+getMovies(PAPI_URL);
+
+function getMovies(url){
+
+    fetch(url).then(res => res.json()).then(data =>{
+        console.log(data.results);
+        showMovies(data.results);
+    })
+}
+
+
+
+
+function showMovies(data){
+
+    main.innerHTML='';
+
+    
+    data.forEach(movie =>{
+        const {title , poster_path , overview , release_date} = movie;
+        const movieE1 = document.createElement('div');
+        movieE1.classList.add('box');
+        movieE1.innerHTML = `
+                <img src="${IMG_URL+poster_path}" alt="${title}">
+                <div class="content">
+                    <h3>${title}</h3>
+                    <p>${overview}</p>
+                    <a href="#topmovies" class="btn">Rate Now</a>
+                </div>
+
+        `
+        main.appendChild(movieE1);
+    })
+     
+}
+
+
